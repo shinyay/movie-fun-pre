4fe7cab69276118d501acb0b2912c78faf475a88
shinyay <shinya.com@gmail.com>
2018-10-16 10:32:38 +0900

    Add method to return LocalContainerEntityManagerFactoryBean for using define EntityManager

--- src/main/java/org/superbiz/moviefun/AlbumsDatabaseConfig.java
+++ src/main/java/org/superbiz/moviefun/AlbumsDatabaseConfig.java
@@ -3,6 +3,8 @@ package org.superbiz.moviefun;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 
@@ -14,5 +16,15 @@ public class AlbumsDatabaseConfig {
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setURL(serviceCredentials.jdbcUrl("albums-mysql"));
         return dataSource;
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean albumsEntityManagerFactory(DataSource albumsDataSource, HibernateJpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(albumsDataSource);
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        factoryBean.setPackagesToScan(DatabaseConfig.class.getPackage().getName());
+        factoryBean.setPersistenceUnitName("albums");
+        return factoryBean;
     }
 }
